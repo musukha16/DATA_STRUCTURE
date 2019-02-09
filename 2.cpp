d60fd575e26b590d615a49c3ae976d5a2b6bdaa7
musukha16 <musukhabasumatary16@gmail.com>
2019-02-09 11:56:30 +0530

    isdiffiult_sentence.cpp

---
+++ 2.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+bool isVowel(char ch)
+ {
+   if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' )
+   	return true;
+   else
+   	false;
+ }
+int difficult(string str1)
+ {
+   
+   int l1,l2,count_vowels=0,count_conson=0,consec_conson=0;
+   int count_hard=0,count_easy=0;
+   l1=str1.length();
+
+///
+
+  for(int i=0;i<l1;i++)
+   	 {
+	       if(str1[i]!=' ' && isVowel(str1[i]))
+	       {
+	       	count_vowels++;
+	       	consec_conson=0;
+	       }
+	       else if (str1[i]!=' ')
+	       {
+	       	count_conson++;
+	       	consec_conson++;
+	       }
+  
+
+ ///
+	      if(consec_conson==4)
+	        {
+
+	          count_hard++; 
+	          while(i<l1 && str1[i]!=' ')
+	     		i++;
+	     	  count_vowels=0;
+	     	  count_conson=0;
+	     	  consec_conson=0;
+	           }
+
+	        else if(str1[i] ==' ' || i==(l1-1))
+	          {
+	     	    count_conson > count_vowels ? count_hard++ : count_easy++;
+	     	    count_vowels=0;
+	     	    count_conson=0;
+	     	  	consec_conson=0;
+	          }
+
+   	   }
+       cout<<count_hard<<endl;
+       cout<<count_easy<<endl;
+      return (5*(count_hard)+3*(count_easy));
+
+ }
+int main()
+ {
+    string str1="I AM A GEEK";
+    string str2="we are GEEKs";
+    cout<<difficult(str1)<<endl;
+    cout<<difficult(str2)<<endl;
+       
+ 	return 0;
+ }
